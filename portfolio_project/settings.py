3a0e5d5c9baef00c8392b67430a097894820d16a
Harshini <bejjankiharshini2@gmail.com>
2026-06-05 00:52:23 +0530

    Add Render deployment setup

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rugfabqa!0*hr@vpb)zte#yqh3r&9$dc*txj(l2!2$qkg0ts-z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
